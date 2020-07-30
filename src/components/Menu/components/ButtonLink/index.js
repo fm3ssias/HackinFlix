1b642f2106da19735349a59123b3c40acd2df879
fm3ssias <filipe.messias@protonmail.com>
2020-07-30 14:39:24 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ButtonLink (props) {
+    // props => { className: "o que alguem passar", href: "oq alguem passar tb" }
+    console.log(props)
+    return (
+        <a className={props.className} href={props.href}>
+            {props.children}
+        </a>
+    );
+}
+
+export default ButtonLink;
